99b3d5fd0f87612283237d9ce90b9b90eb6836e1
RoomofR <alawajiryan@gmail.com>
2025-09-11 06:15:43 +0300

    Setup entire stack similar to bhvr.

--- frontend/src/App.view.ts
+++
@@ -1,9 +0,0 @@
-import { View, Main, div } from "@dlightjs/dlight"
-
-@Main
-@View
-class App {
-  Body() {
-    div("hello dlight")
-  }
-}
---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { serveStatic } from "hono/bun";
+
+// === ROUTES ===
+const app = new Hono();
+app.use(cors());
+
+// --- Test Routes ---
+app.get('/hello', (c) => {
+	return c.text('Hello Hono!')
+});
+
+// --- API Routes ---
+app.get('/api/hello', (c) => {
+	return c.json({message:"hello world"});
+});
+
+// --- Static Files (client build) ---
+app.use("*", serveStatic({ root: "../client/dist" }));
+
+// === SERVER & AUTH ===
+//Serve the server with its routes and tls if needed.
+const ENV: string = process.env.BUILD || "DEV";
+const PORT: number = ENV === "PROD" ? 443 : Number(process.env.PORT);
+export default {
+	port: PORT,
+	fetch: app.fetch,
+	tls: ENV === "PROD"
+		? {
+			key: Bun.file(process.env.TLS_KEY_PATH!),
+    		cert: Bun.file(process.env.TLS_CERT_PATH!)
+		}
+		: undefined,
+}
